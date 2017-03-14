5218d4b01a405c583f32e9cbecef8e61818c66e9
Jose Lorenzo <isleo11@gmail.com>
2017-03-14 12:18:38 +0000

    Arreglados fallos

--- comandos11.sh
+++ comandos11.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #Preparamos el entorno
 rm *cifrado* 2> /dev/null
-rm *descifrado* 2> /dev/null
 
 #Cifrar con CINCO algoritmos simétricos
 echo AES
@@ -18,15 +17,15 @@ openssl enc -e -rc4 -in texto_legible -out texto_cifrado.rc4 -k 12345 -salt -a -
 
 #Descifrar con los CINCO algoritmos simétricos
 #AES
-openssl enc -d -aes-256-cbc -in texto_cifrado.aes -out texto_descifrado.aes -k 12345
+openssl enc -d -aes-256-cbc -in texto_cifrado.aes -out texto_descifrado.aes -k 12345 -a
 #TDES
-openssl enc -d -des-ede3-cbc -in texto_cifrado.tdes -out texto_descifrado.tdes -k 12345
+openssl enc -d -des-ede3-cbc -in texto_cifrado.tdes -out texto_descifrado.tdes -k 12345 -a
 #IDEA
-openssl enc -d -idea-cbc -in texto_cifrado.idea -out texto_descifrado.idea -k 12345
+openssl enc -d -idea-cbc -in texto_cifrado.idea -out texto_descifrado.idea -k 12345 -a
 #RC2
-openssl enc -d -rc2-cbc -in texto_cifrado.rc2 -out texto_descifrado.rc2 -k 12345
+openssl enc -d -rc2-cbc -in texto_cifrado.rc2 -out texto_descifrado.rc2 -k 12345 -a 
 #RC4 (cifrador en flujo)
-openssl enc -d -rc4 -in texto_cifrado.rc4 -out texto_descifrado.rc4 -k 12345
+openssl enc -d -rc4 -in texto_cifrado.rc4 -out texto_descifrado.rc4 -k 12345 -a
 
 #Comprobamos la igualdad de los ficheros
 diff texto_legible texto_descifrado.aes
